b3e69acb29953c5aaa1de9fea48e187ed3ac3b72
ex4to <turretbox@gmail.com>
2022-12-12 21:07:04 +0700

    chore: command and configs

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Telegraf } from 'telegraf';
+import dotenv from 'dotenv';
+
+const { parsed } = dotenv.config()
+
+const bot = new Telegraf(parsed?.BOT_TOKEN || "")
+
+bot.start((ctx) => {
+    const message = `Please use the /id command to receive your id`
+    ctx.reply(message)
+})
+
+bot.command('id', (ctx) => {
+    console.log(ctx.message.from)
+    ctx.reply(`Your id is: ${ctx.message.from.id}`)
+})
+
+bot.command('monitoring',async (ctx) => {
+    try {
+        await fetch('http://localhost:9100/metrics').then((e) => console.log(e))
+        ctx.reply(`Working on`)
+    } catch (e) {
+        console.log(e, 'wild error')
+        ctx.reply('Wild error appeared!')
+    }
+})
+
+bot.launch()
