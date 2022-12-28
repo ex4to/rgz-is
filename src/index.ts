f98ca96569f4d219b398f037d8f7b3583f693174
ex4to <ex4to@icloud.com>
2022-12-28 09:22:17 +0700

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,31 @@
-import { Telegraf } from 'telegraf';
-import dotenv from 'dotenv';
+import { Telegraf } from "telegraf";
+import dotenv from "dotenv";
 
-const { parsed } = dotenv.config()
+const { parsed } = dotenv.config();
 
-const bot = new Telegraf(parsed?.BOT_TOKEN || "")
+const bot = new Telegraf(parsed?.BOT_TOKEN || "");
 
 bot.start((ctx) => {
-    const message = `Please use the /id command to receive your id`
-    ctx.reply(message)
-})
+  const message = `Please use the /id command to receive your id`;
+  ctx.reply(message);
+});
 
-bot.command('id', (ctx) => {
-    console.log(ctx.message.from)
-    ctx.reply(`Your id is: ${ctx.message.from.id}`)
-})
+bot.command("id", (ctx) => {
+  console.log(ctx.message.from);
+  ctx.reply(`Ваш айди пользователя: ${ctx.message.from.id}`);
+});
 
-bot.command('monitoring',async (ctx) => {
-    try {
-        const info = await fetch('http://localhost:9100/metrics').then((e) => e.text())
-        console.log(info)
-	ctx.reply(`Working on`)
-    } catch (e) {
-        console.log(e, 'wild error')
-        ctx.reply('Wild error appeared!')
-    }
-})
+bot.command("monitoring", async (ctx) => {
+  try {
+    const info = await fetch("http://localhost:9100/metrics").then((e) =>
+      e.text()
+    );
+    console.log(info);
+    ctx.reply(`Working on`);
+  } catch (e) {
+    console.log(e, "wild error");
+    ctx.reply("Oops! Error appeared");
+  }
+});
 
-bot.launch()
+bot.launch();
