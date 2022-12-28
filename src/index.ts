47b321822abdc4fc863bba14e62c288ef1897ffb
ex4to <ex4to@icloud.com>
2022-12-28 05:31:05 +0000

    feat: mem and network traffic info

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,42 @@ bot.command("id", (ctx) => {
   ctx.reply(`Ваш айди пользователя: ${ctx.message.from.id}`);
 });
 
-bot.command("monitoring", async (ctx) => {
+bot.command("debug", async (ctx) => {
   try {
     const info = await fetch("http://localhost:9100/metrics").then((e) =>
       e.text()
     );
-    console.log(info);
-    ctx.reply(`Working on`);
+    const totalMemRaw = info
+      .split("\n")
+      .filter((e) => e.includes('node_memory_MemTotal_bytes') && e.charAt(0) !== "#")
+      .join("")
+      .split(" ");
+
+    const availableMemRaw = info
+      .split("\n")
+      .filter((e) => e.includes('node_memory_MemAvailable_bytes') && e.charAt(0) !== "#")
+      .join("")
+      .split(" ");
+
+    const totalMem = Number(totalMemRaw[1]).toFixed(20) / (1024 * 1024 * 1024)
+    const availableMem = Number(availableMemRaw[1]).toFixed(20) / (1024 * 1024 * 1024)
+
+    const networkRaw = 
+	    await fetch(`http://localhost:9090/api/v1/query_range?query=node_network_receive_bytes_total&start=${new Date(Date.now() - 60000 * 2).toISOString()}&end=${new Date().toISOString()}&step=1m`)
+	    .then((e) => e.json())
+ 
+    if (networkRaw.status === 'success') {
+    	console.log(networkRaw?.data?.result)
+    }
+    
+    console.log(networkRaw?.data?.result[0].values);
+    
+    const networkTraffic = (Number(networkRaw?.data?.result[0].values[2][1]) - Number(networkRaw?.data?.result[0].values[0][1])) * 60 * 2 / (1024 * 8)
+
+    ctx.reply(`Total memory: ${totalMem.toFixed(2)} Gb
+Available memory: ${availableMem.toFixed(2)} Gb
+Network traffic: ${networkTraffic.toFixed(2)} kb/s`);
+
   } catch (e) {
     console.log(e, "wild error");
     ctx.reply("Oops! Error appeared");
